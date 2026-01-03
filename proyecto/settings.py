d49b14369803e76f1ac0d7792d38cbc0ca265c0e
Jazmin <jbedoya628@gmail.com>
2026-01-03 12:30:32 -0300

    Configura STATIC_ROOT y corrige rutas para producción

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,8 @@ ROOT_URLCONF = 'proyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['C:\\Users\jbedo\\Desktop\\Mis_entornos\\mi_web\\src\\proyecto\\base\\templates'],
+        'DIRS': [BASE_DIR / 'base' / 'templates'],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +121,10 @@ LOGIN_URL= 'login'
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
